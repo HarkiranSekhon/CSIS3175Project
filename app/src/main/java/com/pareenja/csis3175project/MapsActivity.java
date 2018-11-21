74bed1446628ad4955bba7b01c82b43fc46a95dd
Puneet Pareenja <pareenjapuneet@gmail.com>
2018-11-21 14:44:27 -0800

    Updated google maps key. Implemented com.google.android.gms:play-services-location:7.+

--- app/src/main/java/com/pareenja/csis3175project/MapsActivity.java
+++ app/src/main/java/com/pareenja/csis3175project/MapsActivity.java
@@ -21,6 +21,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -38,8 +39,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng douglasCollege = new LatLng(49.2035681, -122.9126894);
+        mMap.addMarker(new MarkerOptions().position(douglasCollege).title("Douglas College"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(douglasCollege));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(douglasCollege, 12), 3000, null);
     }
 }
